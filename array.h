d1341d0126b2cc0d6c819dcdaaddeda524b65586
IbybfavxbiZlxbyn <volosnikovkolia@gmail.com>
2017-01-07 13:43:36 +0200

    Start working on a project.

---
+++ array.h
@@ -0,0 +1,10 @@
+#ifndef ARRAY_H_INCLUDED
+#define ARRAY_H_INCLUDED
+
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+
+using namespace std;
+
+#endif // ARRAY_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "array.h"
+
+int main()
+{
+
+    return 0;
+}
